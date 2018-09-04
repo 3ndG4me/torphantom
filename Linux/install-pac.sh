0c1f8d5fc880e9c595d7c039e35e8f049bdf90ae
IkerST <ikersanditrejo@gmail.com>
2018-09-04 10:36:25 -0500

    Edit for arch linux

--- Linux/install-pac.sh
+++ Linux/install-pac.sh
@@ -6,7 +6,7 @@ sudo pacman -S --quiet --noconfirm --needed macchanger
 echo "=====> Installing tor bundle "
 sudo pacman -S --quiet --noconfirm --needed tor
 echo "=====> Installing TorPhantom "
-sudo cp torphantom /usr/bin/torphantom
+sudo cp torphantom-arch /usr/bin/torphantom
 sudo chmod +x /usr/bin/torphantom
 echo "=====> Done "
 echo "=====> Open terminal and type 'torphantom' for usage "
